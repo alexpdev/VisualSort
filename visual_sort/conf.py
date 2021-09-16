4023167da860803c3a6a33ba10755051c08a8f11
Alex <alexpdev@protonmail.com>
2021-09-15 17:37:11 -0700

    updated conf

--- visual_sort/conf.py
+++ visual_sort/conf.py
@@ -9,13 +9,13 @@ SCREEN_SIZE =  (.96,.92,0,0)
 # Background Color for screen, defaults to black but can use (r,g,b) values
 BACKGROUND =  "black"
 # Number of animation to perform per screen update  Default = 1
-TRACER =  2
+TRACER =  9
 # The speed of pen drawing the vector animation 0-10. Set to 0 for no animation.
 SPEED = 10
 # Delay for animation to complete.
-DELAY = 1
+DELAY = 0
 # The Vertical Distance added to each progressive color block
-BLOCK_HEIGHT = 6
+BLOCK_HEIGHT = 8
 # Horizantal distance of color block
-BLOCK_WIDTH =  10
+BLOCK_WIDTH =  13
 
